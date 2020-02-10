d2253a62fac7fd10b8bd81d931ca830f07271296
Skye Geraghty <skyetay03@googlemail.com>
2020-02-10 07:46:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word(phrase)
   words = phrase.split(" ")
   new_phrase = ""
   words.collect { |word| new_phrase += word.reverse  + " "}
-  return new_phrase.strip
+  new_phrase.strip
 end
